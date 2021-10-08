fae4811df1cfa61c78daea1f8c0455e9dfd5dc11
Rus <de.mr44@gmail.com>
2021-10-08 22:53:24 +0500

    Added a card model

---
+++ MVC-Storyboard/Card.swift
@@ -0,0 +1,26 @@
+//
+//  Card.swift
+//  MVC-Storyboard
+//
+//  Created by ruslan on 08.10.2021.
+//
+
+import Foundation
+
+struct Card {
+    
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+    
+    static var identifierNumber = 0
+    
+    static func identifierGenerator() -> Int {
+        identifierNumber += 1
+        return identifierNumber
+    }
+    
+    init() {
+        self.identifier = Card.identifierGenerator()
+    }
+}
--- MVC-Storyboard/ViewController.swift
+++ MVC-Storyboard/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var buttonCollection: [UIButton]!
+    @IBOutlet weak var touchLabel: UILabel!
+    
+    let emojiCollection = ["🏄‍♀️", "🎭", "🏄‍♀️", "🎭"]
+    var touches = 0 {
+        didSet {
+            touchLabel.text = "Touches: \(touches)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    func flipButton(emoji: String, button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = .systemBlue
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = .white
+        }
+    }
+    
+    @IBAction func buttonAction(_ sender: UIButton) {
+        touches += 1
+        if let buttonIndex = buttonCollection.firstIndex(of: sender) {
+            flipButton(emoji: emojiCollection[buttonIndex], button: sender)
+        }
+    }
 }
 
