fc3347841177d7706c80838d3bead81d46296406
Rus <de.mr44@gmail.com>
2021-10-09 22:51:39 +0500

    Updated the ConcentrationGame file

--- MVC-Storyboard/Models/ConcentrationGame.swift
+++ MVC-Storyboard/Models/ConcentrationGame.swift
@@ -11,8 +11,25 @@ class ConcentrationGame {
     
     var cards = [Card]()
     
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
     func chooseCard(at index: Int) {
-        
+        if !cards[index].isMatched {
+            if let matchingIndex = indexOfOneAndOnlyFaceUpCard, matchingIndex != index {
+                if cards[index].identifier == cards[matchingIndex].identifier {
+                    cards[index].isMatched = true
+                    cards[matchingIndex].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                for flipDown in cards.indices {
+                    cards[flipDown].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
     }
     
     init(numberOfPairsOfCards: Int) {
